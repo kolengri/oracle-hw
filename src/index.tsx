d87fecf4e07164efd43a551b4bc5503366e59fbf
Grigoriy Kolenko <kolenko@me.com>
2020-03-17 16:49:06 +0100

    Add hot reload

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import ReactDOM from 'react-dom';
+
+import App from './App';
+import './index.css';
+import * as serviceWorker from './serviceWorker';
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+const render = (Component: React.FC) => {
+  return ReactDOM.render(
+    <>
+      <Component />
+    </>,
+    document.getElementById('root')
+  );
+};
+
+render(App);
+
+if ((module as any).hot) {
+  (module as any).hot.accept('./App', () => {
+    const NextApp = require('./App').default;
+    render(NextApp);
+  });
+}
